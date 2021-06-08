e675d923c880149df341f92650fb9dd4bd596c73
Julian Szigethy <julian.szigethy@gmail.com>
2021-06-08 17:17:51 +0200

    added more buttons, keyboard toggles

--- src/App.js
+++ src/App.js
@@ -1,126 +1,170 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
-const buttonsObject = [
-  {
-    press: "q",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Clap%201.mp3",
-    name: "",
-  },
-  {
-    press: "w",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Clap%202.mp3",
-    name: "",
-  },
-  {
-    press: "e",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Conga%20High.mp3",
-    name: "",
-  },
-  {
-    press: "a",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Conga%20Mid.mp3",
-    name: "",
-  },
-  {
-    press: "s",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Conga%20Low.mp3",
-    name: "",
-  },
-  {
-    press: "d",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Cowbell.mp3",
-    name: "",
-  },
-  {
-    press: "z",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Hihat%20Open.mp3",
-    name: "",
-  },
-  {
-    press: "x",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Mid%20Tom.mp3",
-    name: "",
-  },
-  {
-    press: "c",
-    source:
-      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Pop%20Chord%201.mp3",
-    name: "",
-  },
-];
-
-//get raw file from github as source and set the name of each button
-let nameRegEx = /.+\/(.*)\.mp3$/;
-for (let i = 0; i < buttonsObject.length; i++) {
-  buttonsObject[i].name = decodeURI(buttonsObject[i].source).match(
-    nameRegEx
-  )[1];
-  buttonsObject[i].source = buttonsObject[i].source + "?raw=true";
-}
-
-const handleKeyPressWrapper = (setTextFunc) => {
+const handleKeyPressWrapper = (setTextFunc, buttonsarr) => {
   return (e) => {
     try {
       document.getElementById(e.key.toUpperCase()).currentTime = 0;
-      document.getElementById(e.key.toUpperCase()).play();
+      let playPromise = document.getElementById(e.key.toUpperCase()).play();
+      if (playPromise !== undefined) {
+        playPromise
+          .then(() => {})
+          .catch((error) => {
+            console.log("error", error);
+          });
+      }
       let name = "Loading";
-      for (let i = 0; i < buttonsObject.length; i++) {
-        if (buttonsObject[i].press === e.key) {
-          name = buttonsObject[i].name;
+      for (let i = 0; i < buttonsarr.length; i++) {
+        if (buttonsarr[i].press === e.key) {
+          name = buttonsarr[i].name;
           break;
         }
       }
-
       setTextFunc(name);
     } catch {}
   };
 };
 
-const dbHandleClickWrapper = (setTextFunc) => {
+const dbHandleClickWrapper = (setTextFunc, buttonsarr) => {
   return (e) => {
     const customkey = e.target.attributes.customkey.value;
-    const button = buttonsObject[customkey];
+    const button = buttonsarr[customkey];
     const id = button.press.toUpperCase();
     document.getElementById(id).currentTime = 0;
     document.getElementById(id).play();
     setTextFunc(button.name);
   };
 };
-const DrumButtons = (props) => {
+
+const Dropdown = (props) => {
+  let dropdownstyle = {
+    display: "grid",
+    gridGap: "10px",
+    gridTemplateRows: "70px 1fr 1fr 1fr",
+    gridTemplateColumns: "1fr 70px",
+    gridTemplateAreas: '"header ." "row1 row1" "row2 row2" "row3 row3"',
+    justifySelf: "end",
+    alignSelf: "start",
+    width: "80%",
+    backgroundColor: "grey",
+    borderRadius: "5px",
+  };
+
+  let rowstyle = {
+    display: "flex",
+    gridColumn: "1 / 3",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  };
+
+  let row1 = { ...rowstyle };
+  row1["gridArea"] = "row1";
+  let row2 = { ...rowstyle };
+  row2["gridArea"] = "row2";
+  let row3 = { ...rowstyle };
+  row3["gridArea"] = "row3";
+  let titlestyle = {
+    gridArea: "header",
+    justifySelf: "center",
+    alignSelf: "center",
+  };
+
+  const keyboardToggle = () => {
+    props.setappstate((prev) => {
+      let copy = { ...prev };
+      if (copy.keyboard === "qwerty") {
+        copy.keyboard = "qwertz";
+      } else {
+        copy.keyboard = "qwerty";
+      }
+      return copy;
+    });
+  };
+
+  const minimizedToggle = () => {
+    props.setappstate((prev) => {
+      let copy = { ...prev };
+      copy.minimized = !copy.minimized;
+      return copy;
+    });
+  };
+
+  useEffect(() => {
+    let keyboardswitch =
+      document.getElementById("keyboardswitch").children[1].children[0];
+    let minimizedswitch =
+      document.getElementById("minimizedswitch").children[1].children[0];
+
+    if (props.keyboard === "qwertz") {
+      keyboardswitch.checked = true;
+    }
+    if (props.minimized === false) {
+      minimizedswitch.checked = true;
+    }
+  }, [props.keyboard, props.minimized]);
+
   return (
-    <div id="db-wrapper">
-      {buttonsObject.map((elem, i) => {
-        return (
-          <div className="drum-button" key={i}>
-            <button
-              key={elem.press}
-              className="drum-pad"
-              customkey={i}
-              onClick={dbHandleClickWrapper(props.changetext)}
-              id={elem.name}
-            >
-              <span className="front">{elem.press.toUpperCase()}</span>
-              <audio
-                key={elem.press.toUpperCase()}
-                id={elem.press.toUpperCase()}
-                src={buttonsObject[i].source}
-                className="clip"
-              ></audio>
-            </button>
-          </div>
-        );
-      })}
+    <div id="optionsdropdown" style={dropdownstyle}>
+      <h1 style={titlestyle}>Options</h1>
+      <div id="keyboardswitch" className="switch-row" style={row1}>
+        <div>QWERTY</div>
+        <label className="switch">
+          <input type="checkbox" onClick={keyboardToggle}></input>
+          <span className="slider round"></span>
+        </label>
+        <div>QWERTZ</div>
+      </div>
+      <div id="minimizedswitch" className="switch-row" style={row2}>
+        <div>Minimized</div>
+        <label className="switch">
+          <input type="checkbox" onClick={minimizedToggle}></input>
+          <span className="slider round"></span>
+        </label>
+        <div>Maximised</div>
+      </div>
+      {/* <div id="keyboardswitch" className="switch-row" style={row3}>
+        <div>QWERTY</div>
+        <label className="switch">
+          <input type="checkbox"></input>
+          <span className="slider round"></span>
+        </label>
+        <div>QWERTZ</div>
+      </div> */}
     </div>
   );
+};
+
+const Options = (props) => {
+  const [state, setState] = useState(false); //state determines whether its clicked
+  const handleClick = () => {
+    setState(!state);
+  };
+
+  let optionsstyle = {
+    zIndex: "1",
+    backgroundImage: "url('/FCC-Drum-Machine/gear-white.png')",
+    backgroundSize: "90% auto",
+    backgroundPosition: "50% 50%",
+    backgroundRepeat: "no-repeat",
+    borderRadius: "5px",
+    padding: "10px",
+    height: "50px",
+    width: "50px",
+  };
+  if (state) {
+    return (
+      <React.Fragment>
+        <div id="options" style={optionsstyle} onClick={handleClick}></div>
+        <Dropdown
+          minimized={props.minimized}
+          setappstate={props.setappstate}
+          keyboard={props.keyboard}
+        />
+      </React.Fragment>
+    );
+  } else {
+    return <div id="options" style={optionsstyle} onClick={handleClick}></div>;
+  }
 };
 
 const Display = (props) => {
@@ -131,28 +175,122 @@ const Display = (props) => {
   );
 };
 
-let listenerAttached = false;
-function App() {
-  const [state, setState] = useState({ text: "Press a button" });
-  useEffect(() => {
-    console.log("useEffect", state);
+const DrumButtons = (props) => {
+  return (
+    <div id="db-wrapper">
+      {props.buttonsarr.map((elem, i) => {
+        return (
+          <div className="drum-button" key={i}>
+            <button
+              key={elem.press}
+              className="drum-pad"
+              customkey={i}
+              onClick={dbHandleClickWrapper(props.changetext, props.buttonsarr)}
+              id={elem.name}
+            >
+              <span className="front">{elem.press.toUpperCase()}</span>
+              <audio
+                key={elem.press.toUpperCase()}
+                id={elem.press.toUpperCase()}
+                src={props.buttonsarr[i].source}
+                preload="auto"
+                className="clip"
+              ></audio>
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+function App(props) {
+  const [state, setState] = useState({
+    text: "Press a button",
+    minimized: true,
+    keyboard: "qwerty",
   });
+
   const changeDisplayText = (text) => {
-    console.log("state change function called", text);
-    setState({ text: text });
-    console.log("new state", state);
+    setState((prev) => {
+      let copy = { ...prev };
+      copy.text = text;
+      return copy;
+    });
   };
-  if (!listenerAttached) {
+
+  let buttonsarr = [];
+  if (state.minimized) {
+    let keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"];
+    if (state.keyboard !== "qwerty") {
+      keys[keys.indexOf("z")] = "y";
+    }
+    for (let i = 0; i < props.buttonsarr.length; i++) {
+      if (keys.indexOf(props.buttonsarr[i].press) !== -1) {
+        buttonsarr[keys.indexOf(props.buttonsarr[i].press)] =
+          props.buttonsarr[i];
+      }
+    }
+    console.log("final buttons", buttonsarr);
+  } else {
+    buttonsarr = [...props.buttonsarr];
+
+    if (state.keyboard === "qwerty") {
+      let yIndex = -1;
+      let zIndex = -1;
+      for (
+        let i = 0;
+        i < buttonsarr.length && (yIndex === -1 || zIndex === -1);
+        i++
+      ) {
+        if (buttonsarr[i].press === "y") {
+          yIndex = i;
+          continue;
+        } else if (buttonsarr[i].press === "z") {
+          zIndex = i;
+          continue;
+        }
+      }
+      console.log("Indexes y and z", yIndex, zIndex);
+      let zObj = buttonsarr[zIndex];
+      console.log("buttonsArr[zIndex]", buttonsarr[zIndex]);
+      console.log("buttonsArr[yIndex]", buttonsarr[yIndex]);
+      console.log("zObj", zObj);
+      buttonsarr[zIndex] = buttonsarr[yIndex];
+      buttonsarr[yIndex] = zObj;
+      console.log("buttonsarr[yIndex]", buttonsarr[yIndex], "yIndex", yIndex);
+      console.log("final buttonsarr", buttonsarr);
+    }
+
+    console.log("else triggered", buttonsarr);
+  }
+
+  let lastButtonsarr = React.useRef([]);
+  useEffect(() => {
+    lastButtonsarr.current = buttonsarr;
+    // console.log("created", lastButtonsarr.current);
     document.addEventListener(
       "keydown",
-      handleKeyPressWrapper(changeDisplayText)
+      handleKeyPressWrapper(changeDisplayText, lastButtonsarr.current)
     );
-    listenerAttached = true;
-  }
+    return () => {
+      // console.log("removed", lastButtonsarr.current);
+      document.removeEventListener(
+        "keydown",
+        handleKeyPressWrapper(changeDisplayText, lastButtonsarr.current)
+      );
+    }; // eslint-disable-next-line
+  }, [state.minimized, state.keyboard]);
+
   return (
     <div className="App" id="drum-machine">
+      <Options
+        minimized={state.minimized}
+        keyboard={state.keyboard}
+        setappstate={setState}
+      />
       <Display text={state.text} />
-      <DrumButtons changetext={changeDisplayText} />
+      <DrumButtons changetext={changeDisplayText} buttonsarr={buttonsarr} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,124 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const buttonsArr = [
+  {
+    press: "q",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Clap%201.mp3",
+    name: "",
+  },
+  {
+    press: "w",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Clap%202.mp3",
+    name: "",
+  },
+  {
+    press: "e",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Conga%20High.mp3",
+    name: "",
+  },
+  {
+    press: "r",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Conga%20Mid.mp3",
+    name: "",
+  },
+  {
+    press: "t",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Conga%20Low.mp3",
+    name: "",
+  },
+  {
+    press: "z",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Cowbell.mp3",
+    name: "",
+  },
+  {
+    press: "a",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Hihat%20Open.mp3",
+    name: "",
+  },
+  {
+    press: "s",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Mid%20Tom.mp3",
+    name: "",
+  },
+  {
+    press: "d",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Pop%20Chord%201.mp3",
+    name: "",
+  },
+  {
+    press: "f",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Pop%20Chord%202.mp3",
+    name: "",
+  },
+  {
+    press: "g",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Pop%20Chord%203.mp3",
+    name: "",
+  },
+  {
+    press: "y",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Woop.mp3",
+    name: "",
+  },
+  {
+    press: "x",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Snare%201.mp3",
+    name: "",
+  },
+  {
+    press: "c",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Snare%202.mp3",
+    name: "",
+  },
+  {
+    press: "v",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Snare%203.mp3",
+    name: "",
+  },
+  {
+    press: "b",
+    source:
+      "https://github.com/Julian-Sz/FCC-Drum-Machine/blob/main/src/sounds/Snare%204.mp3",
+    name: "",
+  },
+];
+
+//get raw file from github as source and set the name of each button
+try {
+  let nameRegEx = /.+\/(.*)\.mp3$/;
+  for (let i = 0; i < buttonsArr.length; i++) {
+    buttonsArr[i].name = decodeURI(buttonsArr[i].source).match(nameRegEx)[1];
+    buttonsArr[i].source = buttonsArr[i].source + "?raw=true";
+  }
+} catch (e) {
+  console.error("Error in buttonsArr, full message: \n", e);
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App buttonsarr={buttonsArr} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
